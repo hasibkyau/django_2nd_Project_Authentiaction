950e0ae80a4f2965890c5867531ba9d44003eb42
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-16 11:05:45 +0600

    user login part-1

--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from login_app.forms import UserForm, UserInfoForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -33,3 +38,27 @@ def register(request):
 
     diction = {'user_form':user_form, 'user_info_form': user_info_form, 'registered':registered}
     return render(request, 'login_app/register.html', context=diction)
+
+
+def login_page(request):
+    return render(request, 'login_app/login.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('login_app:index'))
+            else:
+                return HttpResponse("Account is not active!")
+
+        else:
+            return HttpResponse("Login Details are Wrong!")
+
+    else:
+        return render(request, 'login_app/login.html', context={})
