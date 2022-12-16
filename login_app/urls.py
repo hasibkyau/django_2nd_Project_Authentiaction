3dea683a1cc8395361123e73bdf672b89d08cda0
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-16 08:53:45 +0600

    added base templates and realtive url

---
+++ login_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path as url
+from django.urls import path
+from login_app import views
+
+app_name = 'login_app'
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
